3499f74141d98ba2a76f3c7d6b0dde8dd5513723
AndrewCowan2024 <AndrewCowan2024>
2021-10-08 09:10:55 -0500

    2

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #WRITE YOUR CODE IN THIS FILE
-def close10(n):
-    if n> 5:   
-        return 10
-    else:
+def close10(x,y):
+    if abs(x-10) < abs(y-10):
+        return x
+    elif abs(x-10) == abs(y-10):
         return 0
+    else: 
+        return y
        
